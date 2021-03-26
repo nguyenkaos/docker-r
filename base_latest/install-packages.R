244fe7af524960edd374eaef761dd3c79cbb4b81
thienam <thienambenbovutru2019@gmail.com>
2021-03-27 01:51:32 +0700

    update

--- base_latest/install-packages.R
+++ base_latest/install-packages.R
@@ -1,2 +1,8 @@
 # Install packages
-install.packages('forecast', repos='https://cran.r-project.org')
+install.packages("forecast")
+install.packages("forecastML")
+install.packages("bsts")
+install.packages("forecastHybrid")
+install.packages("httr")
+install.packages("jsonlite")
+install.packages("candlesticks", repos="http://R-Forge.R-project.org")
